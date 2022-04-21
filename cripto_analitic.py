580d3401426983085529d2f082595a375538e946
god-of-jesters <85024796+god-of-jesters@users.noreply.github.com>
2022-04-21 20:13:12 +0300

    Update cripto_analitic.py

--- cripto_analitic.py
+++ cripto_analitic.py
@@ -7,19 +7,20 @@ window.geometry('400x600')
 
 def day():
     i = 1
-    t = {}
+    t,l = [],[]
     dt = datetime.datetime.now()
     dat = f"{dt.day}.{dt.month}.{dt.year}"
     cursor.execute('Select dat from crip3;')
     q = list(cursor.fetchall())
     cursor.execute('Select curs from crip3;')
     c = list(cursor.fetchall())
-    print(q[-i])
+    cursor.execute('Select time from crip3;')
+    t2 = list(cursor.fetchall())
     while q[-i] != f"{dt.day-1}.{dt.month}.{dt.year}" and len(q)-1 >= i:
-        print(q[-i])
         i += 1
-        t[q[-i]] = c[-i]
-    a = plt.plot(t)
+        t.append(t2[-i][0])
+        l.append(c[-i][0])
+    plt.plot(t[0:-1],l[0:-1])
     plt.show()
 def week():
     pass
